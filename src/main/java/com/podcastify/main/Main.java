eaee1c84b116b821ccba4db523694c6d22470e4b
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-29 00:22:27 +0700

    chore: update endpoint, load port from env

--- src/main/java/com/podcastify/main/Main.java
+++ src/main/java/com/podcastify/main/Main.java
@@ -1,14 +1,21 @@
 package com.podcastify.main;
 
-import javax.xml.ws.Endpoint;
+import com.podcastify.implementor.SubscribeServiceImpl;
 
-import com.podcastify.implementor.PodcastServiceImpl;
+import javax.xml.ws.Endpoint;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://0.0.0.0:5555/api/podcast/subscription", new PodcastServiceImpl());
-            System.out.println("Listen and serve at: http://localhost:5555/api");
+            Dotenv dotenv = Dotenv.load(); 
+            String port = dotenv.get("PORT", "5555");
+
+            // Subscription route
+            Endpoint.publish("http://0.0.0.0:" + port + "/api/v1/subscription", new SubscribeServiceImpl());
+
+
+            System.out.println("Listen and serve at: http://localhost:" + port + "/api/v1");
         } catch (Exception e) {
             e.printStackTrace();
         }
