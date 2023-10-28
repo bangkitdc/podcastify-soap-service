79e6b37aa1783a1dd0425f49adfc906cc3c0ecaa
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-29 00:19:44 +0700

    feat(response): add createResponse for responses

--- src/main/java/com/podcastify/constant/Response.java
+++ src/main/java/com/podcastify/constant/Response.java
@@ -1,5 +1,9 @@
 package com.podcastify.constant;
 
+import com.podcastify.model.ResponseModel;
+
+import java.sql.SQLException;
+
 public class Response {
     public static final int HTTP_STATUS_OK = 200;
     public static final int HTTP_STATUS_CREATED = 201;
@@ -15,4 +19,30 @@ public class Response {
     public static final int HTTP_STATUS_PAYLOAD_TOO_LARGE = 413;
     public static final int HTTP_STATUS_UNSUPPORTED_MEDIA_TYPE = 415;
     public static final int HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+
+    public static ResponseModel createResponse(int statusCode, String message) {
+        ResponseModel response = new ResponseModel();
+        response.setStatusCode(statusCode);
+        response.setMessage(message);
+        return response;
+    }
+
+    public static ResponseModel createResponse(Exception e) {
+        if (e instanceof SecurityException)
+            return Response.createResponse(Response.HTTP_STATUS_UNAUTHORIZED, e.getMessage());
+        if (e instanceof SQLException)
+            return Response.createResponse(Response.HTTP_STATUS_BAD_REQUEST, e.getMessage());
+        else {
+            String errMessage = e.getMessage() == null ? "internal server error" : e.getMessage();
+            return Response.createResponse(Response.HTTP_STATUS_INTERNAL_SERVER_ERROR, errMessage);
+        }
+    }
+
+    public static ResponseModel createResponse(int statusCode, String message, Object data) {
+        ResponseModel response = new ResponseModel();
+        response.setStatusCode(statusCode);
+        response.setMessage(message);
+        response.setData(data);
+        return response;
+    }
 }
